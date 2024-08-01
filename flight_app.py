31373c22d0887b14218c1beab7f8d0df81d86ecc
Mouad AIt Khouya <104041002+mouadsifaw@users.noreply.github.com>
2024-08-01 00:55:01 -0400

    Update flight_app.py

--- flight_app.py
+++ flight_app.py
@@ -6,28 +6,20 @@ import pandas as pd
 try:
     with open('preprocessor.pkl', 'rb') as file:
         preprocessor = pickle.load(file)
-except Exception:
-    st.error("There was a problem loading the preprocessor.")
+except Exception as e:
+    st.error(f"Error loading preprocessor: {e}")
 
 try:
     with open('best_rf_model.pkl', 'rb') as file:
         model = pickle.load(file)
-except Exception:
-    st.error("There was a problem loading the model.")
+except Exception as e:
+    st.error(f"Error loading model: {e}")
 
 # Define the user interface
 st.markdown("<h1 style='text-align: center;'>Flight Delay Prediction</h1>", unsafe_allow_html=True)
 
-# Add Instructions in the Sidebar
-st.sidebar.markdown("""
-### Instructions
-1. **Select the flight details**: Use the dropdown menus to input the flight year, month, day, departure time block, and carrier.
-2. **Click on 'Predict'**: After filling in the details, click the **Predict** button to see if the flight is likely to be delayed.
-3. **View Results**: The result will be displayed below the button.
-""")
-
 # Input fields
-year = st.selectbox('Year', [2023, 2024])
+year = st.selectbox('Year', [2024, 2025])
 month = st.selectbox('Month', list(range(1, 13)))
 day = st.selectbox('Day', list(range(1, 32)))
 dep_time_block = st.selectbox('Departure Time Block', [
@@ -53,16 +45,15 @@ if st.button('Predict'):
     # Preprocess the features
     try:
         preprocessed_features = preprocessor.transform(features)
-    except Exception:
-        st.error("There was a problem processing your input.")
-        st.stop()  # Stop further execution if preprocessing fails
+    except Exception as e:
+        st.error(f"Error preprocessing features: {e}")
     
     # Make prediction
     try:
         prediction = model.predict(preprocessed_features)
         if prediction[0] == 1:
-            st.success("The flight will likely be delayed by 15 minutes or more.")
+            st.write("The flight will likely be delayed by 15 minutes or more.")
         else:
-            st.success("The flight will likely not be delayed by 15 minutes or more.")
-    except Exception:
-        st.error("There was a problem making the prediction.")
+            st.write("The flight will likely not be delayed by 15 minutes or more.")
+    except Exception as e:
+        st.error(f"Error making prediction: {e}")
